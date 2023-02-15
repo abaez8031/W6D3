1d616ddca21b54d3a2fbfc07aa012b191bb303d9
abaez8031 <abaez8031@gmail.com>
2023-02-15 15:26:00 -0500

    third-commit

--- firstroutesandcontrollers/config/routes.rb
+++ firstroutesandcontrollers/config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  resources :users
+  # resources :users
+
+  get '/users', to: 'users#index'
+  get '/users/:id', to: 'users#show'
+  patch '/users/:id', to: 'users#update'
+  put '/users/:id', to: 'users#update'
+  post '/users', to: 'users#create'
+  delete '/users/:id', to: 'users#destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
