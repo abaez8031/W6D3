d03c957e645bb83ae6d70c3f89c870c260e0953c
abaez8031 <abaez8031@gmail.com>
2023-02-16 17:40:48 -0500

    nested routes

--- firstroutesandcontrollers/config/routes.rb
+++ firstroutesandcontrollers/config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :show, :update, :create, :destroy]
-  resources :artworks, only: [:index, :show, :update, :create, :destroy]
+  resources :artworks, only: [:show, :update, :create, :destroy]
   resources :artwork_shares, only: [:index, :create, :destroy]
+  
+  # /users/:user_id/artworks
+  resources :users do
+    resources :artworks
+  end
   
 
   # get '/users', to: 'users#index'
